e880af826ea8b3c533264d725cbb3bf1e79493fb
Anzhelika Poruchnik <poruchnik@gmail.com>
2025-04-08 02:48:01 +0300

    новлено вюшки та HTML-шаблони

---
+++ myapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def page1(request):
+    return render(request, 'page1.html')
+
+def page2(request):
+    return render(request, 'page2.html')
+
+def page3(request):
+    return render(request, 'page3.html')
+
+def page4(request):
+    return render(request, 'page4.html')
